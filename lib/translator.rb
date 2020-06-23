3064b1a6b6970eceb12e2cf91ab82ba6507a5f88
Kiri Um <kirirotha@gmail.com>
2020-06-23 03:16:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,5 +24,17 @@ end
 def get_english_meaning(file_in, jap_emote)
   
   # code goes here
+  emoticons = YAML.load_file(file_in)
+  emoticons_sort = {}
+  emoticons.each do |emotions, emote|
+    #puts emoticons[emotions][0]
+    #puts emoticons[emotions][1]
+    if !emoticons_sort[emotions]
+      emoticons_sort[emotions] = {}
+    end
+    emoticons_sort[emotions][:english] =  emoticons[emotions][0]
+    emoticons_sort[emotions][:japanese] = emoticons[emotions][1]
+  end
+  
 end
 
