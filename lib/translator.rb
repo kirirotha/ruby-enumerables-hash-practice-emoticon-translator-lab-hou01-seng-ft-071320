71ce6989d98d2888e48c1be7444499989733d2e3
Kiri Um <kirirotha@gmail.com>
2020-06-23 03:09:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,8 @@
 # require modules here
 require "yaml"
-file_in = "lib/emoticons.yml"
-def load_library
-  emoticons = YAML.load_file("lib/emoticons.yml")
+file_in = ".lib/emoticons.yml"
+def load_library(file_in)
+  emoticons = YAML.load_file(file_in)
   emoticons_sort = {}
   emoticons.each do |emotions, emote|
     #puts emoticons[emotions][0]
