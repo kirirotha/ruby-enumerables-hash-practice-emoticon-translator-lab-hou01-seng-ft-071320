41d06c7b2614b2510f8b8daff6c762eca2a34ee1
Kiri Um <kirirotha@gmail.com>
2020-06-23 02:31:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,17 @@
 # require modules here
-
+require "yaml"
 def load_library
-  # code goes here
+  emoticons = YAML.load_file('lib/emoticons.yml')
+  emoticons_sort = {}
+  emoticons.each do |emotions, emote|
+    puts emoticons[emotions][0]
+    puts emoticons[emotions][1]
+    if !emoticons_sort[emotions]
+      emoticons_sort[emotions] = {}
+    end
+    #emoiticons_sort[emotions] << :english: emoticons[emotions][0]
+  end
+  puts emoticons_sort
 end
 
 def get_japanese_emoticon
@@ -11,3 +21,5 @@ end
 def get_english_meaning
   # code goes here
 end
+
+load_library
